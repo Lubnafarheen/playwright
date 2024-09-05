6fab1bcafda14239dbab2e5ebf3627e498de171f
lubna farheen <lubna.farheen2010@gmail.com>
2024-09-06 01:14:53 +0200

    Playwright-Interacting with elements Using various Locator Methods

---
+++ tests/Practise/Practise_1.spec.ts
@@ -0,0 +1,30 @@
+import {test} from "@playwright/test";
+
+test.skip('Basic Navigation', async({page}) => {
+    await page.goto('https://gitlab.com/');
+     await page.waitForTimeout(3000);
+     await page.reload();
+})
+
+ test.skip('Interacting with elements on Gitlab', async ({page}) => {
+    await page.goto('https://gitlab.com/');
+    await page.click('#onetrust-accept-btn-handler');
+    await page.locator('.be-nav-navigation').getByRole('link',{name:'Get Free Trial'}).click();
+    await page.locator('[data-testid= "new-user-first-name-field"]').fill('John');
+    await page.locator('[data-testid= "new-user-last-name-field"]').fill('Doe');
+    await page.getByTestId('new-user-username-field').fill('Johnny');
+})
+
+test.only('Using various Locator Methods', async ({page}) => {
+    await page.goto('https://gitlab.com/');
+    await page.click('#onetrust-accept-btn-handler');
+    //await page.getByRole('link', {name: 'Sign in'}).click();
+   // await page.getByRole('button', {name: 'Contact us'}).click();
+    await page.click(':has-text("Solutions")');
+    await page.waitForTimeout(3000);
+})
+
+
+
+
+
